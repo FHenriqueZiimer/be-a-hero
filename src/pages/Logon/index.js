20d8ef2838dc9d7ebaabc6f96775a07d80e39f7d
Fábio <fabohenrique@live.com>
2020-04-12 21:59:37 -0300

    Client side validation :lock:
    -validation created
    -span error created
    -input number mask

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import heoresImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 import { FiLogIn } from 'react-icons/fi';
-import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import swal from 'sweetalert2'
 import api from '../../services/api'
@@ -10,61 +9,113 @@ import api from '../../services/api'
 import './style.scss'
 
 
-function Logon() {
-  const [id, setId] = useState('')
-  const { register, handleSubmit, errors } = useForm();
+class Logon extends Component {
+  constructor(props) {
+    super(props)
 
-  async function handleLogin (_,e) {
-    e.preventDefault();
+    this.state = {
+      id: '',
+      errors: {
+        id: ''
+      }
+    }
 
-    await api.post('login', { id }).then(res => {
-      localStorage.setItem('userId', id); //salvando no browser o id
-      localStorage.setItem('userName', res.data.name)
-
-      swal.fire({
-        icon:'success',
-        title: 'Parabéns!',
-        text: `Olá ${res.data.name}! Seu login foi realizado com sucesso!`
-      })
-    }).catch(error => {
-      swal.fire({
-        icon:'error',
-        text: 'Erro no Login, verifique se o ID está correto'
-      })
-    })
+    this.inputId = React.createRef();
+    this.handleLogin = this.handleLogin.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  return (
-   <div className="logon-container">
-     <section className="form">
-      <img src={logoImg} alt="Logo Be The Hero"/>
+  validateForm () {
+    let valid = true
+    const errorsMessage = {};
 
-      <form onSubmit={handleSubmit(handleLogin)}>
-        <h1>Faça seu Login</h1>
-        <input
-          placeholder="Sua Id"
-          name="id"
-          ref={register({
-            required: 'Por favor insira seu ID'
-            })}
-          value={id}
-          onChange={e => setId(e.target.value)}
-        />
-         <span className="error">
-              {errors.id && errors.id.message}
+    if(this.state.id.replace(/\s/g, '') === '') {
+      valid = false
+      this.inputId.current.style.borderColor = 'red'
+      this.inputId.current.focus()
+      errorsMessage.id = 'Por favor insira o seu ID'
+    }
+
+    if(!valid) {
+      this.setState({
+        errors: Object.assign({}, this.state.errors, errorsMessage)
+      })
+    }
+
+    return valid
+  }
+
+  handleChange (e) {
+    this.setState({ [e.target.name] : e.target.value })
+
+    this.inputId.current.style.borderColor = ''
+      this.setState({
+        errors: ''
+      })
+  }
+
+  async handleLogin (e) {
+    e.preventDefault();
+
+    const isValid = this.validateForm();
+
+    if(isValid) {
+      this.inputId.current.style.borderColor = ''
+      this.setState({
+        errors: ''
+      })
+
+      await api.post('login', { id: this.state.id }).then(res => {
+        localStorage.setItem('userId', this.state.id);
+        localStorage.setItem('userName', res.data.name)
+
+        swal.fire({
+          icon:'success',
+          title: 'Parabéns!',
+          text: `Olá ${res.data.name}! Seu login foi realizado com sucesso!`
+        })
+      }).catch(error => {
+        swal.fire({
+          icon:'error',
+          text: 'Erro no Login, verifique se o ID está correto'
+        })
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="logon-container">
+        <section className="form">
+          <img src={logoImg} alt="Logo Be The Hero"/>
+
+          <form onSubmit={this.handleLogin}>
+            <h1>Faça seu Login</h1>
+
+            <input
+              placeholder="Seu Id"
+              name="id"
+              ref={this.inputId}
+              value={this.state.id}
+              onChange={e => this.handleChange(e)}
+            />
+            <span className="error">
+              {this.state.errors.id}
             </span>
-
-        <button className="button" type="submit">Entrar</button>
-
-        <Link className="back-link" to="/register">
-          <FiLogIn size={16} color="#E02041"/>
-          Não tenho cadastro
-        </Link>
-      </form>
-     </section>
-     <img src={heoresImg} alt="Heroes" />
-   </div>
-  )
+            <button className="button" type="submit">Entrar</button>
+              <Link className="back-link" to="/register">
+                <FiLogIn size={16} color="#E02041"/>
+                  Não tenho cadastro
+              </Link>
+            </form>
+          </section>
+        <img src={heoresImg} alt="Heroes" />
+      </div>
+    )
+  }
 }
+
+
+
 
 export default Logon
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,168 +1,210 @@
-import React, { useState } from 'react';
+import React, { Component }  from 'react';
+import InputMask from 'react-input-mask';
 import { FiArrowLeft } from 'react-icons/fi';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import swal from 'sweetalert2';
-import { useForm } from 'react-hook-form';
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
 import './style.scss';
 
-function Register() {
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [number, setNumber] = useState('');
-  const [city, setCity] = useState('');
-  const [uf, setUf] = useState('');
-
-  const history = useHistory();
-
-  const { register, handleSubmit, errors } = useForm();
-
-
-async function handleRegister(e) {
-  e.preventDefault();
-
-  function validationField() {
-    if(name.replace(' ', '') === '') {
-      const inputName = document.getElementsByName('name')[0]
-      inputName.style.borderColor = "red";
-      return inputName.focus();
-    }
-  }
-
-
-  await validationField();
-    // if(name.replace(' ', '') === '') {
-    //   const inputName = document.getElementsByName('name')[0]
-    //   inputName.style.borderColor = "red";
-    //   inputName.focus();
-    // }
-    // if (email === '' ) {
-    //   const inputEmail = document.getElementsByName('email')[0]
-    //   inputEmail.style.borderColor = "red";
-    //   inputEmail.focus();
-    // }
-    //  else {
-    //   document.getElementsByName('name')[0].style.borderColor = "";
-    //   document.getElementsByName('email')[0].style.borderColor = "";
-    // }
-  const data ={
-    name,
-    email,
-    number,
-    city,
-    uf
-  }
-
-  api.post('user', data).then(res => {
-    swal.fire({
-      text: `Seu id foi criado: ${res.data.id}`,
-      showClass: {
-        popup: 'animated fadeInDown faster'
-      },
-      hideClass: {
-        popup: 'animated fadeOutUp faster'
-    }
-  })
-   history.push('/')
-
-  }).catch(error => {
-    alert(error)
-    swal.fire({
-      icon: 'error',
-      text: 'Erro no cadastro, tente novamente',
-      showClass: {
-        popup: 'animated fadeInDown faster'
-      },
-      hideClass: {
-        popup: 'animated fadeOutUp faster'
+class Register extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      number: '',
+      city: '',
+      uf: '',
+      errors: {
+        name: '',
+        email: '',
+        number: '',
+        uf: '',
       }
-    });
+    };
 
-  })
-}
+    this.inputName = React.createRef();
+    this.inputEmail = React.createRef();
+    this.inputNumber = React.createRef();
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleRegister = this.handleRegister.bind(this);
+  };
+
+  handleChange (e) {
+    const target = e.target;
+
+    this.setState({ [target.name] : target.value });
+
+    target.style.borderColor = '';
+
+    this.setState(prevState => ({
+      errors: {
+        ...prevState.errors,
+        [target.name]: ''
+      }}
+    ));
+   };
+
+  validateRegister () {
+    let valid = true;
+    const errorsMessage = {};
+
+    if(this.state.name.replace(/\s/g, '') === '') {
+      this.inputName.current.style.borderColor = 'red'
+      valid = false
+      errorsMessage.name = 'Nome obrigatório'
+    }
+
+    if(this.state.email.replace(/\s/g, '') === '') {
+      this.inputEmail.current.style.borderColor = 'red'
+      valid = false
+      errorsMessage.email = 'Email obrigatório'
+    }
+
+    if(!this.state.email.includes('@') || !this.state.email.includes('.')) {
+      this.inputEmail.current.style.borderColor = 'red'
+      valid = false
+      errorsMessage.email = 'Email inválido'
+    }
+
+    if(this.state.number ===  '') {
+      this.inputNumber.style.borderColor = 'red'
+      valid = false
+      errorsMessage.number = 'Número obrigatório'
+    }
+
+    if(!valid) {
+      this.setState({
+        errors: Object.assign({}, this.state.errors, errorsMessage)
+      })
+    }
+
+    return valid
+  }
+
+  async handleRegister (e) {
+    e.preventDefault();
+
+    const isValid= this.validateRegister()
+
+    if(isValid) {
+      this.setState({ errors: '' })
+
+      const data = {
+        name: this.state.name,
+        email: this.state.email,
+        number: this.state.number,
+        city: this.state.city,
+        uf: this.state.uf
+      }
+
+     await api.post('user', data).then(res => {
+        swal.fire({
+          text: `Seu id foi criado: ${res.data.id}`,
+          showClass: {
+            popup: 'animated fadeInDown faster'
+          },
+          hideClass: {
+            popup: 'animated fadeOutUp faster'
+        }
+      });
+      this.props.history.push('/');
+      }).catch(error => {
+        console.log(error)
+        swal.fire({
+          icon: 'error',
+          text: 'Erro no cadastro, tente novamente',
+          showClass: {
+            popup: 'animated fadeInDown faster'
+          },
+          hideClass: {
+            popup: 'animated fadeOutUp faster'
+          }
+        });
+      })
+    }
+  }
+
+render () {
   return (
     <div className="register-container">
       <div className="content">
         <section>
           <img src={logoImg} alt="Logo Be The Hero"/>
-
-            <h1>Cadastro</h1>
-            <p>Faça seu cadastro, entre na plataforma e veja como esse projeto funciona!</p>
+             <h1>Cadastro</h1>
+             <p>Faça seu cadastro, entre na plataforma e veja como esse projeto funciona!</p>
 
             <Link className="back-link" to="/">
               <FiArrowLeft size={16} color="#E02041"/>
                 Login
             </Link>
-            </section>
-          <form onSubmit={handleRegister}>
-            <input
-              name="name"
-              placeholder="* Nome"
-              value={name}
-              onChange={e => {setName(e.target.value)}}
-            />
+        </section>
+
+        <form onSubmit={this.handleRegister}>
+          <input
+            name="name"
+            ref={this.inputName}
+            placeholder="* Nome"
+            value={this.state.name}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <span className="error">
+            {this.state.errors.name}
+          </span>
+
+          <input
+            name="email"
+            ref={this.inputEmail}
+            placeholder="* E-mail"
+            value={this.state.email}
+            onChange={(e) => this.handleChange(e)}
+          />
             <span className="error">
-              {errors.name && errors.name.message}
-            </span>
-            <input
-              type="email"
-              name="email"
-              placeholder="* E-mail"
-              ref={register({
-                required: 'Email obrigatório',
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                  message: "Email inválido"
-                }})}
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-            <span className="error">
-              {errors.email && errors.email.message}
+              {this.state.errors.email}
             </span>
 
-            <input
-              placeholder="* WhatsApp"
-              minLength="12"
-              maxLength="12"
-              name="number"
-              ref={register({
-                required: 'Número obrigatório',
-                })}
-              value={number}
-              onChange={e => setNumber(e.target.value.replace(/\D/,''))}
-            />
-             <span className="error">
-              {errors.phoneNumber && errors.phoneNumber.message}
-            </span>
+          <InputMask
+            mask="(99) 9999-99999"
+            name="number"
+            inputRef={(input) => this.inputNumber = input}
+            placeholder="* WhatsApp"
+            value={this.state.number}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <span className="error">
+            {this.state.errors.number}
+          </span>
 
-          <div className="input-group">
-            <input
-              placeholder="Cidade "
-              name="city"
-              value={city}
-              onChange={e => setCity(e.target.value)}
-            />
+        <div className="input-group">
+          <input
+            placeholder="Cidade"
+            name="city"
+            value={this.state.city}
+            onChange={(e) => this.handleChange(e)}
+          />
 
-            <input
-              maxLength="2"
-              minLength="2"
-              placeholder="UF"
-              name="uf"
-              style={{width: 80}}
-              value={uf}
-              onChange={e => setUf(e.target.value.toUpperCase())}
-            />
-          </div>
+          <input
+            maxLength="2"
+            placeholder="UF"
+            name="uf"
+            style={{width: 80}}
+            value={this.state.uf.toUpperCase()}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <span className="error">
+            {this.state.errors.uf}
+          </span>
 
-           <button className="button" type="submit">Cadastrar</button>
+        </div>
+
+          <button className="button" type="submit">Cadastrar</button>
           </form>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Register
